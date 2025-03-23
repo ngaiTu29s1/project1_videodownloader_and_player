e736a30d1bbdddce2da492b71889f83b6c91243c
tudaddy <91655254+tudaddy@users.noreply.github.com>
2025-03-23 21:27:51 +0700

    little adjust

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile
+from PySide6.QtWidgets import QApplication
 from views.py.loginScreen import LoginWindow
-
 
 if __name__ == "__main__":
     app = QApplication()
     window = LoginWindow()
     window.show()
     app.exec()
-
