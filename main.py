aa559d1c95fcf9e7275fbe9cc67ba8884ef45867
tudaddy <91655254+tudaddy@users.noreply.github.com>
2025-03-30 18:08:26 +0700

    update to work with MVC model for login func

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import sys
 from PySide6.QtWidgets import QApplication
 from views.py.loginScreen import LoginWindow
+from models.loginModel import LoginModel
+from controllers.loginController import LoginController
+
+def main():
+    app = QApplication(sys.argv)
+
+    # Initialize the model, view, and controller
+    model = LoginModel()
+    view = LoginWindow()
+    controller = LoginController(model, view)
+
+    # Show the login window
+    view.show()
+
+    # Run the application
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
-    app = QApplication()
-    window = LoginWindow()
-    window.show()
-    app.exec()
+    main()
